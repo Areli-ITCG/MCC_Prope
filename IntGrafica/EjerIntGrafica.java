eac31b7d457c7ee79c2433e7bc22ca19355f477d
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2021-06-30 23:44:33 -0500

    Create EjerIntGrafica.java

---
+++ IntGrafica/EjerIntGrafica.java
@@ -0,0 +1,12 @@
+public class EjerIntGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Acceso acc = new Acceso();
+        acc.setVisible(true);
+    }
+    
+}
